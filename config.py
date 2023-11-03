71582b6befe50a38886fcea730d2680a6e5963ce
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2023-11-03 20:32:39 +0200

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ START_IMG = getenv("START_IMG", "https://telegra.ph/file/2fd19ebe4a0ae1006b767.j
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://https://t.me/NO_VP0")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/NO_VP0")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NO_VP8")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
